03cc30f902d9fb6029f61e684acddbc9ad679473
Amanda Klenk <amandamklenk3@gmail.com>
2022-06-25 15:25:08 -0400

    created db calls to add and delete a candidate

--- server.js
+++ server.js
@@ -17,10 +17,39 @@ const db = mysql.createConnection(
     console.log("Connected to the election database.")
 );
 
-//db object using query method to run the SQL query
+//database
+//call to retrieve candidates data
 db.query(`SELECT * FROM candidates`, (err, rows) => {
     console.log(rows);
-})
+});
+//call to get a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) =>{
+    if(err){
+        console.log(err);
+    }
+    console.log(row);
+});
+//call to delete a candidate
+// db.query(`DELETE FROM candidates where id = ?`, 1, (err, result) => {
+//     if (err){
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+//call to create a candidate
+const sql = `INSERT INTO candidates  (id, first_name, last_name, industry_connected)
+            VALUES (?,?,?,?)`;
+
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err){
+        console.log(err);
+    }
+    console.log(result);
+});
+
+//express
 //this is what happens when a request is not found
 app.use((req, res) => {
 res.status(404).end();
