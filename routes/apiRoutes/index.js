772521886998ba5cd5d4af673a74a872d5e84992
Amanda Klenk <amandamklenk3@gmail.com>
2022-06-27 15:27:43 -0400

    created post route in voteRoutes, routed through index in apiRoutes folder

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 //hub for routes
+router.use(require('./voteRoutes'));
 router.use(require("./candidateRoutes"));
 router.use(require('./partyRoutes'));
 router.use(require('./voterRoutes'));
